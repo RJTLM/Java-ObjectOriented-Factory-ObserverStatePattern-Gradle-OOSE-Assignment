ab6d7d13c62d946a188518b1d890eb7e50cde9cf
Ryan Mackintosh <129054093+RJTLM@users.noreply.github.com>
2024-05-26 15:59:33 +0800

    Implement preliminary BikeFactory ie manage creation of Bike objects.

--- src/main/java/edu/curtin/oose2024s1/assignment2/factory/BikeFactory.java
+++ src/main/java/edu/curtin/oose2024s1/assignment2/factory/BikeFactory.java
@@ -1,5 +1,17 @@
 package edu.curtin.oose2024s1.assignment2.factory;
 
+import edu.curtin.oose2024s1.assignment2.model.Bike;
+/*
+Purpose:
+    - The BikeFactory class will manage the creation of Bike instances. This centralises the object creation process, ensuring that all Bike objects are created in a consistent manner.
+Role:
+    - As a factory class, it simplifies the creation of Bike objects throughout the application, particularly when new bikes are added to the inventory. It abstracts the instantiation logic away from the main application logic, promoting a clean separation of concerns.
+ */
 // Creates new Bike instances.
-public class BikeFactory {
+public class BikeFactory
+{
+    public Bike createBike()
+    {
+        return new Bike();
+    }
 }
