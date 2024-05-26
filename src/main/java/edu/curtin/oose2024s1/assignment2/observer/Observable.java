95e140f6ddbb9fe7c88afb02fc3f3028b462c23e
Ryan Mackintosh <129054093+RJTLM@users.noreply.github.com>
2024-05-26 16:05:30 +0800

    Implement preliminary Observer Patter package ie updates the view whenever there is a change in the model.

--- src/main/java/edu/curtin/oose2024s1/assignment2/observer/Observable.java
+++ src/main/java/edu/curtin/oose2024s1/assignment2/observer/Observable.java
@@ -1,5 +1,15 @@
 package edu.curtin.oose2024s1.assignment2.observer;
 
+/*
+Purpose:
+    - Defines the contract for the subject that maintains a list of observers and notifies them of changes.
+Role:
+    - Provides methods to add, remove, and notify observers.
+ */
 // Interface for observable objects.
-public interface Observable {
+public interface Observable
+{
+    void addObserver(Observer observer);
+    void removeObserver(Observer observer);
+    void notifyObservers();
 }
--- src/main/java/edu/curtin/oose2024s1/assignment2/observer/Observer.java
+++ src/main/java/edu/curtin/oose2024s1/assignment2/observer/Observer.java
@@ -1,5 +1,12 @@
 package edu.curtin.oose2024s1.assignment2.observer;
 
+/*
+Purpose:
+    - Defines the contract for observer objects that need to be notified of changes.
+Role:
+    - Provides a method to update the observer, used by the observable to notify observers of changes.
+ */
 // Interface for observers.
 public interface Observer {
+    void update();
 }
